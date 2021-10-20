8ab9f92458c704c290245304b02e88ba69f799ad
Yura <yura007k@gmail.com>
2021-10-20 13:05:51 +0300

    rollback

--- src/config.js
+++ src/config.js
@@ -7,16 +7,11 @@ function getConfig(env) {
   case 'mainnet':
     return {
       networkId: 'mainnet',
-      nodeUrl: 'https://rpc.testnet.near.org',
+      nodeUrl: 'https://rpc.mainnet.near.org',
       contractName: CONTRACT_NAME,
-      walletUrl: 'https://wallet.testnet.near.org',
-      helperUrl: 'https://helper.testnet.near.org',
-      explorerUrl: 'https://explorer.testnet.near.org',
-//       nodeUrl: 'https://rpc.mainnet.near.org',
-//       contractName: CONTRACT_NAME,
-//       walletUrl: 'https://wallet.near.org',
-//       helperUrl: 'https://helper.mainnet.near.org',
-//       explorerUrl: 'https://explorer.mainnet.near.org',
+      walletUrl: 'https://wallet.near.org',
+      helperUrl: 'https://helper.mainnet.near.org',
+      explorerUrl: 'https://explorer.mainnet.near.org',
     }
   case 'development':
   case 'testnet':
